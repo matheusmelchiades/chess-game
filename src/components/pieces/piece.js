d4a335cae3a8ac60dbfa5cb83de82b50f56e7fa9
matheus maciel <matheusmmelchiades@gmail.com>
2026-02-28 06:12:47 +1000

    refactor:base piece class with color, position and sprite support

--- src/components/pieces/piece.js
+++ src/components/pieces/piece.js
@@ -1,67 +1,31 @@
 class Piece {
-  constructor() {
+  constructor(color, col, row) {
+    this.color = color;
+    this.col = col;
+    this.row = row;
     this.sprite = {
       path: 'src/components/pieces/sprites',
       ext: 'png',
       data: null,
     };
     this.size = 30;
-    this.coordinates = {
-      x: null,
-      y: null,
-    };
-    this.position = {
-      start: {
-        x: 0,
-        y: 0,
-      },
-      current: {
-        x: 0,
-        y: 0,
-      },
-    };
+    this.hasMoved = false;
   }
 
-  draw() {
-    if (this.sprite.data && this.coordinates.x && this.coordinates.y) {
-      imageMode(CENTER);
-      image(this.sprite.data, this.coordinates.x + this.size, this.coordinates.y + this.size, this.size, this.size);
+  loadSprite(name) {
+    this.sprite.data = loadImage(`${this.sprite.path}/${this.color}/${name}.${this.sprite.ext}`);
+  }
+
+  draw(x, y, squareSize) {
+    if (this.sprite.data) {
+      const padding = squareSize * 0.1;
+      imageMode(CORNER);
+      image(this.sprite.data, x + padding, y + padding, squareSize - padding * 2, squareSize - padding * 2);
     }
   }
 
-  moveUp() {
-    const { x, y } = this.position.current;
-    const target = y - 1;
-
-    if (target >= 0) {
-      this.position.current = { x, y: target };
-    }
-  }
-
-  moveRight() {
-    const { x, y } = this.position.current;
-    const target = x + 1;
-
-    if (target < 8) {
-      this.position.current = { x: target, y };
-    }
-  }
-
-  moveLeft() {
-    const { x, y } = this.position.current;
-    const target = x - 1;
-
-    if (target >= 0) {
-      this.position.current = { x: target, y };
-    }
-  }
-
-  moveDown() {
-    const { x, y } = this.position.current;
-    const target = y + 1;
-
-    if (target < 8) {
-      this.position.current = { x, y: target };
-    }
+  // Returns array of { col, row } objects for candidate moves (no legality check)
+  getMoves(_board, _enPassantTarget) {
+    return [];
   }
 }
